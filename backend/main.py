c30319f4d817232f01e7d8e7bb5abbb3c86e0eb7
Gitesh <giteshzope19@gail.com>
2025-12-04 16:31:38 +0530

    feat: integrate Gemini client for moment identification in video transcripts

--- backend/main.py
+++ backend/main.py
@@ -20,6 +20,7 @@ from google import genai
 import pysubs2
 from tqdm import tqdm
 import whisperx
+from google import genai
 
 
 class ProcessVideoRequest(BaseModel):
@@ -61,6 +62,10 @@ class AiPodcastClipper:
         )
         print("Transcription models loaded...")
 
+        print("Creating gemini client")
+        self.gemini_client = genai.Client(api_key=os.environ["GEMINI_API_KEY "])
+        print("Created gemini client...")
+
     def transcribe_video(self, base_dir: str, video_path: str) -> str:
         audio_path = base_dir / "audio.wav"
         extract_cmd = f"ffmpeg -i {video_path} -vn -acodec pcm_s16le -ar 16000 -ac 1 {audio_path}"
@@ -84,7 +89,7 @@ class AiPodcastClipper:
 
         duration = time.time() - start_time
         print("Transcription and alignment took " + str(duration) + " seconds")
-        print(json.dumps(result, indent=2))
+        # print(json.dumps(result, indent=2))
         segments = []
 
         if "word_segments" in result:
@@ -96,6 +101,31 @@ class AiPodcastClipper:
                 })
 
         return json.dumps(segments)
+    
+    def identify_moments(self, transcript: dict):
+        response = self.gemini_client.models.generate_content(model="gemini-2.5-flash-preview-04-17", contents="""
+    This is a podcast video transcript consisting of word, along with each words's start and end time. I am looking to create clips between a minimum of 30 and maximum of 60 seconds long. The clip should never exceed 60 seconds.
+
+    Your task is to find and extract stories, or question and their corresponding answers from the transcript.
+    Each clip should begin with the question and conclude with the answer.
+    It is acceptable for the clip to include a few additional sentences before a question if it aids in contextualizing the question.
+
+    Please adhere to the following rules:
+    - Ensure that clips do not overlap with one another.
+    - Start and end timestamps of the clips should align perfectly with the sentence boundaries in the transcript.
+    - Only use the start and end timestamps provided in the input. modifying timestamps is not allowed.
+    - Format the output as a list of JSON objects, each representing a clip with 'start' and 'end' timestamps: [{"start": seconds, "end": seconds}, ...clip2, clip3]. The output should always be readable by the python json.loads function.
+    - Aim to generate longer clips between 40-60 seconds, and ensure to include as much content from the context as viable.
+
+    Avoid including:
+    - Moments of greeting, thanking, or saying goodbye.
+    - Non-question and answer interactions.
+
+    If there are no valid clips to extract, the output should be an empty list [], in JSON format. Also readable by json.loads() in Python.
+
+    The transcript is as follows:\n\n""" + str(transcript))
+        print(f"Identified moments response: ${response.text}")
+        return response.text
 
     @modal.fastapi_endpoint(method="POST")
     def process_video(self, request: ProcessVideoRequest, token: HTTPAuthorizationCredentials = Depends(auth_scheme)):
@@ -116,10 +146,29 @@ class AiPodcastClipper:
             "ai-podcast-clipper-videos", s3_key, str(video_path))
 
         # 1. Transcription
-        transcript_sgements_json = self.transcribe_video(base_dir, video_path)
-        transcript_sgements = json.loads(transcript_sgements_json)
+        transcript_segments_json = self.transcribe_video(base_dir, video_path)
+        transcript_segments = json.loads(transcript_segments_json)
 
         # 2. Identify Moments for clips
+        print("Identifying clip moments")
+        identified_moments_raw = self.identify_moments(transcript_segments)
+
+        cleaned_json_string = identified_moments_raw.strip()
+        if cleaned_json_string.startswith("```json"):
+            cleaned_json_string = cleaned_json_string[len("```json"):].strip()
+        if cleaned_json_string.endswith("```"):
+            cleaned_json_string = cleaned_json_string[:-len("```")].strip()
+
+        clip_moments = json.loads(cleaned_json_string)
+        if not clip_moments or not isinstance(clip_moments, list):
+            print("Error: Identified moments is not a list")
+            clip_moments = []
+
+        print(clip_moments)
+
+        
+
+
 
 
 @app.local_entrypoint()
