4f859168c52179598ae49ef5759e95f2b160b7e1
Gitesh <giteshzope19@gail.com>
2026-01-26 15:54:35 +0530

    test: number of video clips paramater reduce

--- backend/ytdownload.py
+++ backend/ytdownload.py
@@ -3,8 +3,9 @@ from pytubefix.cli import on_progress
 
 url1 = "https://www.youtube.com/watch?v=SOG0GmKts_I"
 url2 = "https://www.youtube.com/watch?v=-vMgbJ6WqN4"
+url3 = "https://www.youtube.com/watch?v=ke3Pb9_oMrg"
 
-yt = YouTube(url2, on_progress_callback=on_progress)
+yt = YouTube(url3, on_progress_callback=on_progress)
 print(yt.title)
 
 ys = yt.streams.get_highest_resolution()
